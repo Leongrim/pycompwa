959b8c049a63be00766df9173090039add205365
Peter Weidenkaff <weidenka@uni-mainz.de>
2019-10-10 13:24:09 +0200

    Test release: 0.0.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readme():
 
 
 setup(name='pycompwa',
-      version='0.0.4',
+      version='0.0.5',
       author='The ComPWA team',
       maintainer="Peter Weidenkaff",
       maintainer_email="weidenka@uni-mainz.de",
