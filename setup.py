9769e63b90024ef1db5e7f2b1184574e0c4e9be7
Peter Weidenkaff <weidenka@uni-mainz.de>
2019-10-10 14:38:17 +0200

    TravisCI: fix deploy stage

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readme():
 
 
 setup(name='pycompwa',
-      version='0.0.6',
+      version='0.0.7',
       author='The ComPWA team',
       maintainer="Peter Weidenkaff",
       maintainer_email="weidenka@uni-mainz.de",
