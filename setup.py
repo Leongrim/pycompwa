97294e35944febcd44ddae6876ac0d2cccdbf140
Peter Weidenkaff <weidenka@uni-mainz.de>
2019-10-10 12:53:44 +0200

    TravisCI: fix pypi encrypted password

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readme():
 
 
 setup(name='pycompwa',
-      version='0.0.2',
+      version='0.0.3',
       author='The ComPWA team',
       maintainer="Peter Weidenkaff",
       maintainer_email="weidenka@uni-mainz.de",
